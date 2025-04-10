370e042ee4e1dd4e0d71d459bf7c04091d4ff6c2
David Köppl <78260527+Masdaofdisasda@users.noreply.github.com>
2025-04-10 18:26:54 +0200

    Improve build (#5)
    * lazy loading
    
    * update deps

--- client/src/components/ThreeCanvas.tsx
+++ client/src/components/ThreeCanvas.tsx
@@ -4,6 +4,8 @@ import ParticleSimulation, {UniformProps} from "./ParticleSimulation.tsx";
 import {OrbitControls} from "@react-three/drei";
 import {Label, PARTICLE_COUNT} from "./DjPoseApp.types.ts";
 import {memo, RefObject} from "react";
+import {EffectComposer, HueSaturation, ToneMapping} from "@react-three/postprocessing";
+import {AgXToneMapping} from "three";
 
 export type ThreeCanvasProps = {
     detectedLabel: RefObject<Label>;
@@ -17,11 +19,22 @@ const ThreeCanvas = memo(function ThreeCanvasComponent({
     uniforms
     }: ThreeCanvasProps) {
     return (<Canvas
-        camera={{position: [0.0, 0.0, 2.0]}}
+        camera={{position: [0.0, 0.0, 2.0], near: 0.1, far: 100}}
     >
-        <Perf position="top-left" style={{opacity: isDebug ? 1 : 0, transition: 'opacity 0.5s'}} />
+        {import.meta.env.DEV && <Perf position="top-left" style={{opacity: isDebug ? 1 : 0, transition: 'opacity 0.5s'}} />}
         <ParticleSimulation uniforms={uniforms} size={PARTICLE_COUNT} label={detectedLabel}/>
         <OrbitControls autoRotate />
+        <EffectComposer >
+            {/*<DepthOfField
+                focusDistance={0.05}
+                focalLength={0.02}
+                bokehScale={2}
+                target={[0, 0, 0]}
+            />*/}
+
+            <HueSaturation hue={0.0} saturation={0.0} />
+            <ToneMapping mode={AgXToneMapping} />
+        </EffectComposer>
     </Canvas>)
 });
 
