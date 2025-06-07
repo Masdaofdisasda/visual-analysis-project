928328be197a06ca9fa4fa0f993c845a352ad6e4
David Köppl <78260527+Masdaofdisasda@users.noreply.github.com>
2025-06-07 10:31:55 +0200

    fix labels (#7)

--- client/src/hooks/usePoseDetection.tsx
+++ client/src/hooks/usePoseDetection.tsx
@@ -1,4 +1,4 @@
-import {useMemo, useRef} from "react";
+import {useEffect, useMemo, useRef, useState} from "react";
 import usePoseEstimation from "./usePoseEstimation.tsx";
 
 const labelColors: Record<string, string> = {
@@ -12,7 +12,17 @@ function usePoseDetection(
 ) {
     const videoRef = useRef<HTMLVideoElement>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const detectedLabel = usePoseEstimation(videoRef, canvasRef);
+    const detectedLabelRef = usePoseEstimation(videoRef, canvasRef);
+    const [detectedLabel, setDetectedLabel] = useState<string>(detectedLabelRef.current);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (detectedLabelRef.current !== detectedLabel) {
+                setDetectedLabel(detectedLabelRef.current);
+            }
+        }, 100); // Adjust interval as needed
+        return () => clearInterval(interval);
+    }, [detectedLabelRef, detectedLabel]);
 
     const debugCanvas = useMemo(function DebugCanvas() {
         return (
@@ -31,13 +41,13 @@ function usePoseDetection(
             <div
                 className={`flex flex-col items-center justify-center absolute bottom-0 left-0 p-2`}
                 style={{
-                    backgroundColor: labelColors[detectedLabel.current],
+                    backgroundColor: labelColors[detectedLabel],
                     opacity: isDebug ? 1 : 0,
                     transition: 'opacity 0.5s'
                 }}
             >
                 <h2 className="text-2xl font-semibold mb-6">
-                    Detected Pose: <span className="capitalize">{detectedLabel.current}</span>
+                    Detected Pose: <span className="capitalize">{detectedLabel}</span>
                 </h2>
 
                 {debugCanvas}
@@ -45,7 +55,7 @@ function usePoseDetection(
         )
     }, [debugCanvas, detectedLabel, isDebug]);
 
-    return { detectedLabel, debugOverlay };
+    return { detectedLabel: detectedLabelRef, debugOverlay };
 }
 
 export default usePoseDetection;
